3da098a389e798227ecf819fa2629e19447f8be0
FoxSinOfGreed1729 <70275323+FoxSinOfGreed1729@users.noreply.github.com>
2021-05-04 19:08:48 +0530

    Prerequisites

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,3 +1,6 @@
 apt install python3-pip git libnfnetlink-dev libnetfilter-queue-dev
+echo "[+] netfilterqueue dependancies installed"
 pip3 install -U git+https://github.com/kti/python-netfilterqueue
+echo "[+] netfilterqueue installed"
 pip3 install scapy
+echo "[+] scapy installed"
